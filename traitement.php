a3ec5b71e7a8ac8388873d9bb9e469403aceb651
romulus31 <romain.fauconnier@yahoo.fr>
2013-01-22 13:55:30 +0000

    traitement.php

---
+++ traitement.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+<title> 
+	Traitement
+</title>
+</head>
+
+<body>
+	<?php include("includes/header.php");?>
+	
+	
+	<?php include("includes/menu.php");?>
+	
+
+	
+	
+	<?php include("includes/footer.php");?>
+</body>
+
+</html>
