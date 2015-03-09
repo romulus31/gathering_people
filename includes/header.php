ce629686043928396b8e6694a5ceeab5309a5826
fauconnierr <fauconnierr@FR-D23367.groupinfra.com>
2015-03-09 15:48:36 +0100

    Bootstrap added

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@ session_start();
 //				 
 //}	?>
 <div id="header" class="container">
-    <div  class="page-header">
-			<h1><a href="index.php">GATHERING PEOPLE</a></h1>
+    <div  class="page-header ">
+        <h1><a href="index.php">GATHERING PEOPLE </a> <small>place to organize, change, learn...</small></h1>
 	</div>
 </div>
